cff5eb0b9a8f12f1a365a6ae048df34765447c8b
h110401 <hung110401@gmail.com>
2022-07-31 22:25:06 +0700

    bt1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(2);
+
+        System.out.println(circle.getRadius());
+
+        System.out.println(circle.getArea());
+
+    }
+}
